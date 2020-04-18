2dfbb3b09015e9532610704d6824d9dd3ffac8fe
ZioSaba <sabatini.1794627@studenti.uniroma1.it>
2020-04-18 16:32:16 +0200

    Co-authored-by: Loldlink <the.anarky98@gmail.com>

--- VGdatabase/app/controllers/games_controller.rb
+++ VGdatabase/app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
 skip_before_action :verify_authenticity_token
 
 def homepage
-    @user_id = session[:user_id]
+    @user = User.find(session[:user_id]) 
 end
 
 def index
