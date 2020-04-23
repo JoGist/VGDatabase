136b9892d1ffd04b34b41862bda8cdafa6c6e01f
ZioSaba <sabatini.1794627@studenti.uniroma1.it>
2020-04-23 11:54:05 +0200

    add to library

--- VGdatabase/app/controllers/mylibraries_controller.rb
+++ VGdatabase/app/controllers/mylibraries_controller.rb
@@ -2,12 +2,14 @@ class MylibrariesController < ApplicationController
     skip_before_action :verify_authenticity_token
     def create
         game_id = params[:game_id]
+        @games = Game.find(game_id)
         user_id = session[:user_id]
-        @game = Game.find(game_id)
+        @user = User.find(session[:user_id])
         @mylibrary = Mylibrary.new(:favorite => 'false')
-        @mylibrary.user_id = user_id
-        @mylibrary.game_id = game_id
-        @mylibrary.save
+        @mylibrary.user_id = @user.id
+        @mylibrary.game_id = @games.id
+        @mylibrary.save!
         redirect_to game_path(game_id)
+        end
     end
 end
