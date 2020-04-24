4eab93fe2bd22d5e920a7c487277621682de6aa4
Giovanni Roma <r.giovanni.98@gmail.com>
2020-04-24 18:50:15 +0200

    Fixed reviews visual

--- VGdatabase/app/controllers/games_controller.rb
+++ VGdatabase/app/controllers/games_controller.rb
@@ -45,6 +45,8 @@ skip_before_action :verify_authenticity_token
 
     def editProfile
         @user = User.find(session[:user_id])
+        @review = Review.where(:user_id => @user)
+        @review = Review.where(:game_id => @games)
     end
 
     def editingProfile
