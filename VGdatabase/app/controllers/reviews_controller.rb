e1936bd5a9c45d162530cf70dfae3483658e8b63
Loldlink <the.anarky98@gmail.com>
2020-04-24 16:50:18 +0200

    review ok

--- VGdatabase/app/controllers/reviews_controller.rb
+++ VGdatabase/app/controllers/reviews_controller.rb
@@ -9,7 +9,7 @@ def create
     game_id = params[:game_id]
     user_id = session[:user_id]
     score = params[:review][:score]
-    comments = params[:review][:comments]
+    comments = params[:review][:comment]
     @user = User.find(user_id)
     @game = Game.find(game_id)
     @review = Review.new(:score => score,:comments => comments)
@@ -18,36 +18,29 @@ def create
     @review.save!
     redirect_to game_path(@game)
 end
-    def index
-        movie_id = params[:movie_id]
-        user_id = session[:user_id]
-        @reviews = Review.where(:movie_id => movie_id)
-        @movie = Movie.find(movie_id)
-    end
 
-    def index1
-        user_id = session[:user_id]
-        @user = Moviegoer.find(user_id)
-        @reviews = Review.where(:moviegoer_id => user_id)
-    end
+def edit
+    id = params[:id]
+    game_id = params[:game_id]
+    @review = Review.find(id)
+    @game = Game.find(game_id)
+end
 
-    def edit
-        id = params[:id]
-        movie_id = params[:movie_id]
-        @reviews = Review.find(id)
-        @movie = Movie.find(movie_id)
-	end
+def update
+    id = params[:id]
+    game_id = params[:game_id]
+    score = params[:review][:score]
+    comments = params[:review][:comment]
+    @reviews = Review.find(id)
+    @reviews.update_attributes!(:score => score, :comments => comments)
+    redirect_to game_path(game_id)
+end
 
-	def update
-        id = params[:id]
-        @reviews = Review.find(id)
-        @reviews.update_attributes!(params[:review].permit(:potatoes, :comments))
-        redirect_to index_path
-	end
-
-    def destroy
-		id = params[:id]
-		@reviews = Review.find(id)
-		@reviews.destroy
-	end
+def destroy
+    id = params[:id]
+    @reviews = Review.find(id)
+    @reviews.destroy
+    game = params[:game_id]
+    redirect_to game_path(game)
+end
 end
