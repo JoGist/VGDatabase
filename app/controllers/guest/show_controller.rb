dd99e49ac1ce89132e991f0590c60784de35f3b1
Loldlink <the.anarky98@gmail.com>
2020-05-15 19:51:52 +0200

    guest show

--- app/controllers/guest/show_controller.rb
+++ app/controllers/guest/show_controller.rb
@@ -6,6 +6,11 @@ module Guest
             id = params[:id]
             @game = Game.find(id)
             @review = Review.where(:game_id => @game)
+            api_endpoint = 'https://api-v3.igdb.com/games'
+            request_headers = { headers: { 'user-key' => Rails.application.credentials.maps[:igdb] } }
+            api = Apicalypse.new(api_endpoint, request_headers)
+            api.fields(:summary).where(:id => @game.serial).request
+            @plot = api.request[0].values[1]
         end
     end
 end
